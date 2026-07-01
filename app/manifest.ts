1e2baacf626cc3d10557a705dabb89d7f3f850ae
Prince Khunt <princekhuntdatastructure@gmail.com>
2026-07-01 22:21:19 +0530

    favicon, logo added

--- app/manifest.ts
+++ app/manifest.ts
@@ -22,15 +22,16 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: 'any',
       },
       {
-        src: '/icon.svg',
-        sizes: 'any',
-        type: 'image/svg+xml',
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/apple-icon',
-        sizes: '180x180',
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
